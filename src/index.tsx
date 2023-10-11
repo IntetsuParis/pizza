94013e300e564a6c8d5afdd1a96bd264d5a961f1
IntetsuParis <heripahka@mail.ru>
2023-10-11 23:37:08 +0300

    123

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='git'>
     <Provider store={store}>
       <App />
     </Provider>
