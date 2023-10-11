2d80cf6745fcf1516e4c3200514d1f110dfcc354
IntetsuParis <heripahka@mail.ru>
2023-10-11 22:28:10 +0300

    changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-
 import ReactDOM from 'react-dom/client';
 import { store } from './redux/store';
 import App from './App';
+import { Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="https://intetsuparis.github.io/git/">
     <Provider store={store}>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </Provider>
   </BrowserRouter>,
 );
