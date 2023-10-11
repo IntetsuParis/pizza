b614cab2b46c16eaefe620987bfdb2320828a52d
IntetsuParis <heripahka@mail.ru>
2023-10-11 22:33:58 +0300

    123

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { store } from './redux/store';
 import App from './App';
-import { Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -9,9 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <BrowserRouter basename="https://intetsuparis.github.io/git/">
     <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </Provider>
   </BrowserRouter>,
 );
